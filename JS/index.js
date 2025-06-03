8006fa0eeee058c89d5852f7fdf88309c379dcde
cuber-feng <2017feng35@gmail.com>
2025-06-03 15:20:54 +0800

    debug

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,15 @@
 function toggleMenu() {
-    const menu = document.getElementById('dropdown-menu');
-    menu.classList.toggle('show');
+  const menu = document.getElementById('dropdown-menu');
+  menu.classList.toggle('show');
 }
 
 //close menu if clicking outside
-['click', 'touchstart'].forEach(eventName => {
-  window.addEventListener(eventName, function (e) {
-    // 如果点击的既不是按钮，也不是菜单内部，就关闭
-    if (
-      !e.target.closest('.dropdown-toggle') &&
-      !e.target.closest('.dropdown-menu')
-    ) {
-      document.getElementById('dropdown-menu').classList.remove('show');
-    }
-  });
+window.addEventListener('click', function (e) {
+  if (
+    !e.target.closest('.dropdown-toggle') &&
+    !e.target.closest('.dropdown-menu')
+  ) {
+    document.getElementById('dropdown-menu').classList.remove('show');
+  }
 });
+
